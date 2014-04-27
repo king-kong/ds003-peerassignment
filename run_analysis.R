c9728ad28d4d2bb07d64051efeb4b4a3c4f862b6
king-kong <robert.kok@gmail.com>
2014-04-27 17:29:15 -0400

    use rbind to combine files instead

--- run_analysis.R
+++ run_analysis.R
@@ -6,15 +6,12 @@ library(knitr)
 library(plyr)
 
 ## 1) Combine test and training data  
-system('mkdir "UCI HAR Dataset/combined"')
-system('mkdir "UCI HAR Dataset/combined/Inertial Signals"')
-
-system('cat "UCI HAR Dataset/train/subject_train.txt" "UCI HAR Dataset/test/subject_test.txt" > "UCI HAR Dataset/combined/subject_test.txt"')
-system('cat "UCI HAR Dataset/train/X_train.txt" "UCI HAR Dataset/test/X_test.txt" > "UCI HAR Dataset/combined/X_test.txt"')
-system('cat "UCI HAR Dataset/train/y_train.txt" "UCI HAR Dataset/test/y_test.txt" > "UCI HAR Dataset/combined/y_test.txt"')
+##  removded
 
 ## 2) Load X_test.txt combined version
-cset <- read.table(file="UCI HAR Dataset/combined/X_test.txt")
+cset <- rbind(read.table(file="UCI HAR Dataset/train/X_train.txt"), read.table(file="UCI HAR Dataset/test/X_test.txt")) 
+label <- rbind(read.table(file="UCI HAR Dataset/train/y_train.txt"), read.table(file="UCI HAR Dataset/test/y_test.txt")) 
+subjects <- rbind(read.table(file="UCI HAR Dataset/train/subject_train.txt"), read.table(file="UCI HAR Dataset/test/subject_test.txt")) 
 
 ## 3) Read the features.txt to get column name, use the loaded values to label cset
 cnames <- read.table('UCI HAR Dataset/features.txt')
@@ -24,7 +21,6 @@ colnames(cset) <- cnames[2][[1]]
 cset_mean_only <- cset[,grep('(-mean[(][)])|(-std[(][)])', names(cset))]
 
 ## 5) merge the labels
-labels <- read.table(file="UCI HAR Dataset/combined/y_test.txt")
 label_values <- read.table('~/datascience/ds-003/week3/UCI HAR Dataset/activity_labels.txt')
 labels_merged <- merge(labels, label_values, by.x="V1", by.y="V1", all=TRUE)
 
@@ -32,10 +28,13 @@ labels_merged <- merge(labels, label_values, by.x="V1", by.y="V1", all=TRUE)
 cset_mean_only["Activity"] <- labels_merged[2][[1]]
 
 ## 7) add subject
-subjects <- read.table("UCI HAR Dataset/combined/subject_test.txt")
 cset_mean_only["Subjects"] <- subjects[1][[1]]
 
 ## 8) summarize - mean of each variable by subject and activity
 library(reshape2)
+library(plyr)
 m <- melt(cset_mean_only, id=c('Subjects','Activity'))
 result <- ddply(m, c('Subjects', 'Activity', 'variable'), summarize, mean = mean(value))
+write.table(result, file="./result.txt", sep="\t", row.names=FALSE)
+
+result
